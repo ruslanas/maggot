42358e054ea69959f26ef958642dae36def86789
Ruslanas Balčiūnas <ruslanas.com@gmail.com>
2017-03-03 19:46:00 +0200

    Stupid addressing fixed

--- emu.bat
+++ emu.bat
@@ -1,1 +1,4 @@
-qemu-system-i386 -d guest_errors -drive format=raw,file=maggot.bin
+@echo off
+IF "%~1"=="" GOTO end
+qemu-system-i386 -d guest_errors -drive format=raw,file=%~1
+:end
--- maggot.asm
+++ maggot.asm
@@ -1,19 +1,16 @@
+
 include 'macros.inc'
 
 ;format ELF
 ;section '.text' writable executable
 
-_kernel_base equ 0x6000
+_kernel_base equ 0x5000
 
 include 'boot.inc'				  ; 512 bytes
 
 use32
-
-macro vga_char arg1 {
-	mov edi, 0xb8000
-	mov byte [edi + 2], arg1
-	mov byte [edi + 3], 00001111b
-}
+org _kernel_base
+kernel_start:
 
 __flush:
 	mov ax, 0x10
@@ -22,18 +19,18 @@ __flush:
 	mov fs, ax
 	mov gs, ax
 	mov ss, ax
-	jmp 0x18:test_program + _kernel_base	  ; jump to user space
+	jmp 0x18:test_program	   ; jump to user space
 __recover:
-	mov edi, 0xb8000
-	mov dword[edi + 6], 0x0C6b0C4f		  ; Ok
+	lea esi, byte[oops]
+	call print
 	jmp $
-
+__success:
+	jmp $
 include "idt.inc"
 include "gdt.inc"
 
 test_program:
-	mov eax, 0x0f210f21
-	mov ebx, 0
+	lea esi, byte[hello]
 	call print
 
 	;mov eax, 0				  ; test interrupt service routines
@@ -41,11 +38,14 @@ test_program:
 	;dw 0xFFFF				  ; invalid op
 	;jmp 0x24:0xffff			   ; tts
 
-	call read
-	call fork
+	;call fork
 	call exit
+
+hello	db 'Protected mode',0
+done	db 'Task completed!!!',0
+oops	db 'Some error occured!',0
 
 include 'lib.inc'
 
-db 1024 * 4 - $ dup 0				  ; align
-db 1020 * 1024 dup 0				  ; vboxmanage wants bigger files
+db kernel_start + 1024 * 4 - $ dup 0				 ; align
+db 1020 * 1024 + 512 dup 0				; vboxmanage wants bigger files
